a6858099a8a2c11b8da77db8f6c0d9bbd3e6acce
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2017-08-13 16:55:35 +0300

    start working on stats

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,29 @@ import collections
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.rides = {}
+    app.rides = {
+        '1': [{
+            'user_id': '1',
+            'from_lat': '30.745392',
+            'from_lon': '-73.978364',
+            'to_lat': '41.308273',
+            'to_lon': '-72.927887'
+        },
+        {
+            'user_id': '1',
+            'from_lat': '40.745392',
+            'from_lon': '-73.978364',
+            'to_lat': '41.308273',
+            'to_lon': '-72.927887'
+        }],
+        '2':[{
+            'user_id': '2',
+            'from_lat': '50.745392',
+            'from_lon': '-73.978364',
+            'to_lat': '41.308273',
+            'to_lon': '-72.927887'
+        }]
+    }
     app.N = 10
 
     @app.route('/')
@@ -22,5 +44,10 @@ def create_app(config_name):
             app.rides[user_id].appendleft(payload)
 
         return jsonify({'status': 'success'}), 201
+
+    @app.route('/api/v1/stats', methods=['GET'])
+    def stats():
+        all_records = [record for user_id in app.rides for record in app.rides[user_id]]
+        return jsonify(all_records)
 
     return app
