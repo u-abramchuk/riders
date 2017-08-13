59059617912cc58495d6cfc83e4d7b9160169c3d
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2017-08-13 23:47:32 +0300

    working on stats

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import collections
+import pandas
 
 
 def create_app(config_name):
@@ -7,24 +8,24 @@ def create_app(config_name):
     app.rides = {
         '1': [{
             'user_id': '1',
-            'from_lat': '30.745392',
-            'from_lon': '-73.978364',
-            'to_lat': '41.308273',
-            'to_lon': '-72.927887'
+            'from_lat': 30.745392,
+            'from_lon': -73.978364,
+            'to_lat': 41.308273,
+            'to_lon': -72.927887
         },
         {
             'user_id': '1',
-            'from_lat': '40.745392',
-            'from_lon': '-73.978364',
-            'to_lat': '41.308273',
-            'to_lon': '-72.927887'
+            'from_lat': 40.745392,
+            'from_lon': -73.978364,
+            'to_lat': 41.308273,
+            'to_lon': -72.927887
         }],
         '2':[{
             'user_id': '2',
-            'from_lat': '50.745392',
-            'from_lon': '-73.978364',
-            'to_lat': '41.308273',
-            'to_lon': '-72.927887'
+            'from_lat': 50.745392,
+            'from_lon': -73.978364,
+            'to_lat': 41.308273,
+            'to_lon': -72.927887
         }]
     }
     app.N = 10
@@ -48,6 +49,11 @@ def create_app(config_name):
     @app.route('/api/v1/stats', methods=['GET'])
     def stats():
         all_records = [record for user_id in app.rides for record in app.rides[user_id]]
-        return jsonify(all_records)
+        df = pandas.DataFrame.from_records(all_records)
+
+        output = make_response(df.to_csv(index=False))
+        output.headers["Content-Disposition"] = "attachment; filename=stats.csv"
+        output.headers["Content-type"] = "text/csv"
+        return output
 
     return app
