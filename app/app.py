e9233f1c8c76b5647dffc11bc1b438111bfcdc77
Uladzimir Abramchuk <abramchuk@targetprocess.com>
2017-08-17 21:46:30 +0300

    add distance calculation

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify, make_response
 import collections
 import pandas
+import math
 from scipy.spatial.distance import pdist
+from .calc import distance
 
 
 def create_app(config_name):
@@ -17,6 +19,8 @@ def create_app(config_name):
     def store():
         payload = request.get_json()
         user_id = str(payload['user_id'])
+        payload['distance'] = distance(
+            payload['from_lat'], payload['from_lon'], payload['to_lat'], payload['to_lon'])
 
         if not user_id in app.rides:
             app.rides[user_id] = {
@@ -24,23 +28,19 @@ def create_app(config_name):
                 'total_rides': 1
             }
         else:
-            app.rides[user_id]['rides'].appendleft(payload)
+            app.rides[user_id]['rides'].append(payload)
             app.rides[user_id]['total_rides'] += 1
 
         return jsonify({'status': 'success'}), 201
 
     def _get_stats(app):
         all_records = [
-            record for user_id in app.rides for record in app.rides[user_id]['rides']]
+            record for user_id in app.rides
+            for record in app.rides[user_id]['rides']
+        ]
         df = pandas.DataFrame.from_records(all_records)
-        dist = df.apply(lambda x: pdist(
-            [[x.from_lat, x.from_lon], [x.to_lat, x.to_lon]])[0], axis=1)
-        # dist = ((df['from_lat'] - df['to_lat'])**2 +
-        #         (df['from_lon'] - df['to_lon'])**2)**0.5
-        if not dist.empty:
-            df = df.assign(dist=dist).sort_values('dist').drop('dist', axis=1)
 
-        return df
+        return df.sort_values('distance').drop('distance', axis=1)
 
     @app.route('/api/v1/stats', methods=['GET'])
     def stats():
@@ -56,4 +56,4 @@ def create_app(config_name):
 
     @app.route('/api/v1/chart', method=['GET'])
     def chart():
-        pass 
+        pass
--- app_tests.py
+++ app_tests.py
@@ -3,13 +3,18 @@ import json
 from app import create_app
 import pandas
 from scipy.spatial.distance import pdist
+import random
+import math
 
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.single_record = {
+
+    def test_store(self):
+        """Test store"""
+        single_record = {
             'user_id': '1',
             'from_lat': 40.745392,
             'from_lon': -73.978364,
@@ -17,55 +22,81 @@ class AppTestCase(unittest.TestCase):
             'to_lon': -72.927887
         }
 
-    def test_store(self):
-        """Test store"""
-        response = self._post('/api/v1/store', self.single_record)
+        response = self._post('/api/v1/store', single_record)
+
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(self.app.rides['1']['rides'][0], self.single_record)
+
         self.assertEqual(self.app.rides['1']['total_rides'], 1)
+
+        record = self.app.rides['1']['rides'][0]
+        self.assertEqual(record['from_lat'], single_record['from_lat'])
+        self.assertEqual(record['from_lon'], single_record['from_lon'])
+        self.assertEqual(record['to_lat'], single_record['to_lat'])
+        self.assertEqual(record['to_lon'], single_record['to_lon'])
+        self.assertAlmostEqual(record['distance'], 108.0859922018123)
 
     def test_store_no_more_than_N(self):
         """Test store more than N records"""
+
+        def get_randomized_ride():
+            return {
+                'user_id': '1',
+                'from_lat': 40.745392 + random.random(),
+                'from_lon': -73.978364 + random.random(),
+                'to_lat': 41.308273 + random.random(),
+                'to_lon': -72.927887 + random.random()
+            }
+
+        records = [get_randomized_ride() for i in range(0, self.app.N + 1)]
+
         for i in range(0, self.app.N + 1):
-            response = self._post('/api/v1/store', self.single_record)
+            single_record = records[i]
+            response = self._post('/api/v1/store', single_record)
             self.assertEqual(response.status_code, 201)
+
+        for i in range(0, self.app.N):
+            actual = self.app.rides['1']['rides'][i]
+            expected = records[i + 1]
+            self.assertEqual(actual['from_lat'], expected['from_lat'])
+            self.assertEqual(actual['from_lon'], expected['from_lon'])
+            self.assertEqual(actual['to_lat'], expected['to_lat'])
+            self.assertEqual(actual['to_lon'], expected['to_lon'])
         self.assertEqual(len(self.app.rides['1']['rides']), self.app.N)
-        self.assertEqual(self.app.rides['1']['total_rides'], self.app.N+1)
+        self.assertEqual(self.app.rides['1']['total_rides'], self.app.N + 1)
 
     def test_stats(self):
-        self.app.rides = {
-            '1': {
-                'rides': [{
-                    'user_id': '1',
-                    'from_lat': 30.745392,
-                    'from_lon': -73.978364,
-                    'to_lat': 41.308273,
-                    'to_lon': -72.927887
-                },
-                    {
-                    'user_id': '1',
-                    'from_lat': 40.745392,
-                    'from_lon': -73.978364,
-                    'to_lat': 41.308273,
-                    'to_lon': -72.927887
-                }],
-                'total_rides': 2
+        rides = [
+            {
+                'user_id': '1',
+                'from_lat': 39.745392,
+                'from_lon': -73.978364,
+                'to_lat': 41.308273,
+                'to_lon': -72.927887
             },
-            '2': {
-                'rides': [{
-                    'user_id': '2',
-                    'from_lat': 50.745392,
-                    'from_lon': -73.978364,
-                    'to_lat': 41.308273,
-                    'to_lon': -72.927887
-                }],
-                'total_rides': 1
+            {
+                'user_id': '1',
+                'from_lat': 40.745392,
+                'from_lon': -73.978364,
+                'to_lat': 41.308273,
+                'to_lon': -72.927887
+            },
+            {
+                'user_id': '2',
+                'from_lat': 41.945392,
+                'from_lon': -73.978364,
+                'to_lat': 41.308273,
+                'to_lon': -72.927887
             }
-        }
+        ]
         self.app.N = 10
-        expected = b'from_lat,from_lon,to_lat,to_lon,user_id\n40.745392,-73.978364,41.308273,-72.927887,1\n50.745392,-73.978364,41.308273,-72.927887,2\n30.745392,-73.978364,41.308273,-72.927887,1\n'
+
+        self._post('/api/v1/store', rides[0])
+        self._post('/api/v1/store', rides[1])
+        self._post('/api/v1/store', rides[2])
 
         response = self.client().get('/api/v1/stats')
+
+        expected = b'from_lat,from_lon,to_lat,to_lon,user_id\n40.745392,-73.978364,41.308273,-72.927887,1\n41.945392,-73.978364,41.308273,-72.927887,2\n39.745392,-73.978364,41.308273,-72.927887,1\n'
 
         self.assertEqual(response.data, expected)
 
